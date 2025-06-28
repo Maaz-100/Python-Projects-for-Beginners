c50e0ff59a3b45d3f59b447bdce17a0615271d3b
Maaz-100 <Smmaazjamil3@gmail.com>
2025-06-28 09:56:37 +0100

    Delete directory

--- teaching moiz/main.py
+++
@@ -1,9 +0,0 @@
-name = input("Name plz ")
-print("ur name is " + name)
-
-age = input("age plz ")
-print("so ur age is " + age)
-
-Date_of_brith = input("Date of Birth ")
-
-print("so ur dATE OF BIRTH IS "+ Date_of_brith )
